2bb574dd2e93a03dda2021bfd3bc983996621cd3
J_LJQ <36041971+Jason-LJQ@users.noreply.github.com>
2024-12-02 11:56:10 -0500

    1

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -11,32 +11,32 @@ public class QueryProcessor {
         }
 
         if (query.contains("your name")) {
-            return "Jason2";
+            return "Team12";
         }
 
-        // Get all numbers in the query
-        // e.g. Which of the following numbers is the largest: 1, 8, 90?
-        if (query.contains("largest")) {
-            String[] numbers = query.split(":")[1].split(",");
-            int max = Integer.MIN_VALUE;
-            for (String number : numbers) {
-                int n = Integer.parseInt(number.trim());
-                if (n > max) {
-                    max = n;
-                }
-            }
-            return String.valueOf(max);
-        }
-
-        // e.g. What is 5 plus 3
-        if (query.contains("plus")) {
-            String[] numbers = query.split("plus")[1].split(" ");
-            int sum = 0;
-            for (String number : numbers) {
-                sum += Integer.parseInt(number.trim());
-            }
-            return String.valueOf(sum);
-        }
+//        // Get all numbers in the query
+//        // e.g. Which of the following numbers is the largest: 1, 8, 90?
+//        if (query.contains("largest")) {
+//            String[] numbers = query.split(":")[1].split(",");
+//            int max = Integer.MIN_VALUE;
+//            for (String number : numbers) {
+//                int n = Integer.parseInt(number.trim());
+//                if (n > max) {
+//                    max = n;
+//                }
+//            }
+//            return String.valueOf(max);
+//        }
+//
+//        // e.g. What is 5 plus 3
+//        if (query.contains("plus")) {
+//            String[] numbers = query.split("plus")[1].split(" ");
+//            int sum = 0;
+//            for (String number : numbers) {
+//                sum += Integer.parseInt(number.trim());
+//            }
+//            return String.valueOf(sum);
+//        }
 
 
         return "";
